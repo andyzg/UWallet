1ca0c6dfacaf4cad6bf2245c5e96fec0bf075690
Seikun Kambashi <seikun_lee@hotmail.com>
2013-11-10 05:31:25 -0500

    Added all fragments and respective xml files, finally finished converting loginFragment from activity to fragment. Implemented Scott and Jesslayout features. Fragment switching still in progress.

--- UWallet/gen/com/enghack/uwallet/R.java
+++ UWallet/gen/com/enghack/uwallet/R.java
@@ -8,6 +8,12 @@
 package com.enghack.uwallet;
 
 public final class R {
+    public static final class anim {
+        public static final int card_flip_left_in=0x7f040000;
+        public static final int card_flip_left_out=0x7f040001;
+        public static final int card_flip_right_in=0x7f040002;
+        public static final int card_flip_right_out=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,46 +23,79 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int uwallet2=0x7f020001;
+        public static final int about_icon=0x7f020000;
+        public static final int about_icon_clicked=0x7f020001;
+        public static final int balance_icon=0x7f020002;
+        public static final int balance_icon_clicked=0x7f020003;
+        public static final int button_about=0x7f020004;
+        public static final int button_balance=0x7f020005;
+        public static final int button_logout=0x7f020006;
+        public static final int button_transaction=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int logout_icon=0x7f020009;
+        public static final int logout_icon_clicked=0x7f02000a;
+        public static final int main_menu_logo=0x7f02000b;
+        public static final int transactions_icon=0x7f02000c;
+        public static final int transactions_icon_clicked=0x7f02000d;
+        public static final int uwallet=0x7f02000e;
+        public static final int uwallet2=0x7f02000f;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int fragment_container=0x7f080000;
+        public static final int about_button=0x7f0a0008;
+        public static final int action_settings=0x7f0a000a;
+        public static final int balance_button=0x7f0a0006;
+        public static final int fragment_container=0x7f0a0000;
+        public static final int imageView1=0x7f0a0001;
+        public static final int login_button=0x7f0a0004;
+        public static final int logout_button=0x7f0a0009;
+        public static final int main_menu_logo=0x7f0a0005;
+        public static final int password_input=0x7f0a0003;
+        public static final int transactions_button=0x7f0a0007;
+        public static final int username_input=0x7f0a0002;
+    }
+    public static final class integer {
+        public static final int card_flip_time_full=0x7f060001;
+        public static final int card_flip_time_half=0x7f060000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_login=0x7f030001;
-        public static final int fragment_transaction=0x7f030002;
+        public static final int fragment_about=0x7f030001;
+        public static final int fragment_balance=0x7f030002;
+        public static final int fragment_login=0x7f030003;
+        public static final int fragment_menu=0x7f030004;
+        public static final int fragment_transaction=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050009;
-        public static final int login=0x7f050007;
-        public static final int password=0x7f050006;
-        public static final int subtitle=0x7f050005;
-        public static final int title_activity_display_message=0x7f050008;
-        public static final int title_activity_main=0x7f050004;
-        public static final int user_name=0x7f050002;
-        public static final int username_input=0x7f050003;
+        public static final int about_us=0x7f07000a;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070009;
+        public static final int login_button=0x7f070007;
+        public static final int password=0x7f070006;
+        public static final int subtitle=0x7f070005;
+        public static final int title_activity_display_message=0x7f070008;
+        public static final int title_activity_main=0x7f070004;
+        public static final int user_name=0x7f070002;
+        public static final int username_input=0x7f070003;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -70,10 +109,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+        public static final int ImageButton=0x7f080002;
     }
 }
