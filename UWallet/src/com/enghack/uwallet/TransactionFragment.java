a6be68b8c66dd1fd623e50f7a34a667699b2d0af
Andy <zhangandy94@gmail.com>
2013-11-10 14:00:12 -0500

    Parsed the date into the Transaction History. Improved the layout

--- UWallet/src/com/enghack/uwallet/TransactionFragment.java
+++ UWallet/src/com/enghack/uwallet/TransactionFragment.java
@@ -6,6 +6,8 @@ import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,7 +34,7 @@ public class TransactionFragment extends Fragment implements OnClickListener {
 	private Listener mListener;
 	private TableLayout table;
 	private ArrayList<Transaction> list;
-	private int textSize = 13;
+	private int textSize = 15;
 
 	public interface Listener {
 	}
@@ -53,47 +55,69 @@ public class TransactionFragment extends Fragment implements OnClickListener {
 		list = MainActivity.getList();
 	}
 
+	@SuppressWarnings("null")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		View v = inflater.inflate(R.layout.fragment_transaction, container,
 				false);
+		String temporaryDate = "0";
 		ScrollView contain = (ScrollView) v.findViewById(R.id.history_contain);
 
+		int width = getActivity().getWindowManager().getDefaultDisplay().getWidth(); 
 		TableLayout.LayoutParams tableParams = new TableLayout.LayoutParams(
 				TableLayout.LayoutParams.MATCH_PARENT,
 				TableLayout.LayoutParams.WRAP_CONTENT);
 		table = new TableLayout(getActivity());
 		table.setLayoutParams(tableParams);
+		
 		TableRow.LayoutParams lparams = new TableRow.LayoutParams(
-				TableRow.LayoutParams.WRAP_CONTENT,
+				width/2,
 				TableRow.LayoutParams.MATCH_PARENT);
-		lparams.weight = 0.1F;
+		
+		lparams.weight = 0.25F;
 		lparams.gravity = Gravity.LEFT;
 		lparams.bottomMargin = 5;
 		lparams.topMargin = 5;
+		
+		TableRow.LayoutParams dateParams = new TableRow.LayoutParams(
+				TableRow.LayoutParams.MATCH_PARENT,
+				TableRow.LayoutParams.MATCH_PARENT);
+		
+		dateParams.weight = 0.30F;
+		dateParams.gravity = Gravity.LEFT;
+		dateParams.topMargin = 25;
+		dateParams.rightMargin = 30;
 
 		for (Transaction trans : list) {
 			if (filterDate(trans)) {
+				if (temporaryDate.equals("0") || !trans.getDate().equals(temporaryDate))
+				{
+					temporaryDate = trans.getDate();
+					TextView date = new TextView(getActivity());
+					SpannableString spanString = new SpannableString(temporaryDate);
+					spanString.setSpan(new UnderlineSpan(), 0, spanString.length(), 0);
+					date.setText(spanString);
+					date.setTextSize(textSize+10);
+					table.addView(date,dateParams);
+					
+				}
 				TableRow row = new TableRow(getActivity());
-				TextView price, date, terminal;
+				TextView price, terminal;
 				price = new TextView(getActivity());
-				date = new TextView(getActivity());
 				terminal = new TextView(getActivity());
 
-				price.setText(String.format("%.2f", trans.getAmount()));
-				date.setText(trans.getDate());
+				price.setText(String.format("%.2f$", trans.getAmount()));
 				terminal.setText(trans.getTerminal());
 
 				// terminal.setWidth(width/2);
 
 				price.setTextSize(textSize);
-				date.setTextSize(textSize);
 				terminal.setTextSize(textSize);
 
 				row.addView(price, lparams);
-				row.addView(date, lparams);
+				// row.addView(date, lparams);
 				row.addView(terminal, lparams);
 				table.addView(row, new TableLayout.LayoutParams(
 						LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
