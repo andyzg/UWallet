8dae698fe4090d538bbc91d5bdbd6a93fef4d499
Seikun Kambashi <seikun_lee@hotmail.com>
2013-11-10 03:34:16 -0500

    added all fragment classes

--- UWallet/gen/com/enghack/uwallet/R.java
+++ UWallet/gen/com/enghack/uwallet/R.java
@@ -25,14 +25,16 @@ public final class R {
         public static final int uwallet2=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080005;
         public static final int imageView1=0x7f080000;
         public static final int login_button=0x7f080003;
         public static final int password_input=0x7f080002;
+        public static final int test_button=0x7f080004;
         public static final int username_input=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_transaction=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
