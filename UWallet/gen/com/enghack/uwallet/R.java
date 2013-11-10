5d2755499fb2b9ddd6be15d683595419d83386d2
Andy <zhangandy94@gmail.com>
2013-11-10 05:46:21 -0500

    Add authentication

--- UWallet/gen/com/enghack/uwallet/R.java
+++ UWallet/gen/com/enghack/uwallet/R.java
@@ -78,28 +78,23 @@ containing a value of this type.
         public static final int uwallet2=0x7f02000f;
     }
     public static final class id {
-<<<<<<< HEAD
-        public static final int action_settings=0x7f080003;
-        public static final int fragment_container=0x7f080000;
-        public static final int header=0x7f080002;
-        public static final int transaction_table=0x7f080001;
-=======
         public static final int about_button=0x7f0a0008;
-        public static final int action_settings=0x7f0a000a;
+        public static final int action_settings=0x7f0a000c;
         public static final int balance_button=0x7f0a0006;
         public static final int fragment_container=0x7f0a0000;
+        public static final int header=0x7f0a000b;
         public static final int imageView1=0x7f0a0001;
         public static final int login_button=0x7f0a0004;
         public static final int logout_button=0x7f0a0009;
         public static final int main_menu_logo=0x7f0a0005;
         public static final int password_input=0x7f0a0003;
+        public static final int transaction_table=0x7f0a000a;
         public static final int transactions_button=0x7f0a0007;
         public static final int username_input=0x7f0a0002;
     }
     public static final class integer {
         public static final int card_flip_time_full=0x7f060001;
         public static final int card_flip_time_half=0x7f060000;
->>>>>>> fragment
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
