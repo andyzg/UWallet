8194c1349b2c9ce39d4c470aa970f19158fe7987
Andy <zhangandy94@gmail.com>
2013-11-10 00:14:38 -0500

    Created the class for parsing HTML data

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a1/8040153fc14900131a8f8914752e4bf7
@@ -0,0 +1,5 @@
+package com.enghack.uwallet.login;
+
+public class HTMLParser {
+	
+}
--- UWallet/gen/com/enghack/uwallet/R.java
+++ UWallet/gen/com/enghack/uwallet/R.java
@@ -22,9 +22,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int uwallet2=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int imageView1=0x7f080000;
+        public static final int login_button=0x7f080003;
+        public static final int password_input=0x7f080002;
+        public static final int username_input=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +40,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int hello_world=0x7f050009;
+        public static final int login=0x7f050007;
+        public static final int password=0x7f050006;
+        public static final int subtitle=0x7f050005;
+        public static final int title_activity_display_message=0x7f050008;
+        public static final int title_activity_main=0x7f050004;
+        public static final int user_name=0x7f050002;
+        public static final int username_input=0x7f050003;
     }
     public static final class style {
         /** 
