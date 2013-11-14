b5ac08bd3012b0de5d495752bee4431836671c6d
Andy <zhangandy94@gmail.com>
2013-11-14 00:37:25 -0500

    Added test notifications. Waiting for sync version to be out so that notifications can be called

---
+++ UWallet/src/com/enghack/uwallet/NotificationReceiverActivity.java
@@ -0,0 +1,12 @@
+package com.enghack.uwallet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationReceiverActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+} 
